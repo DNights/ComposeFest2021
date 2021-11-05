aac72c20f9673b2f38900606c2f09c6bfb99f998
Hyun Dong Han <guehgk456@gmail.com>
2021-11-05 12:29:39 +0900

    [1주차] Jetpack Compose Codelab 프로젝트 생성

---
+++ week 1-Jetpack Compose basics/app/src/main/java/dev/dnights/composecodelab1/MainActivity.kt
@@ -0,0 +1,38 @@
+package dev.dnights.composecodelab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import dev.dnights.composecodelab1.ui.theme.ComposeCodelab1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCodelab1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCodelab1Theme {
+        Greeting("Android")
+    }
+}
